f9cef5fa34e521a584673639f0704de55ac867da
Myron Walker <myron.walker@gmail.com>
2023-03-16 21:51:47 -0700

    Move exceptionator over to mojo-xmodules

---
+++ source/tests/test_enhancer.py
@@ -0,0 +1,33 @@
+import unittest
+
+from mojo.xmodules.xexceptions import (
+    enhance_exception,
+    format_exception
+)
+
+class TestError(Exception):
+    """
+        This is a test error used for testing the exceptionator.
+    """
+
+def failing_function():
+    raise TestError("This is a test error")
+
+class TestEnhancer(unittest.TestCase):
+
+    def test_enhance_exception(self):
+
+        try:
+            failing_function()
+        except TestError as terr:
+            enhance_exception(terr, "This is some extra content.", "BLAH")
+
+            tblines = format_exception(terr)
+
+            for line in tblines:
+                print(line)
+
+        return
+
+if __name__ == '__main__':
+    unittest.main()
