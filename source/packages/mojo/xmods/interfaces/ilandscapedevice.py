5fa3d0d3f069272721bf5e3aecfeda73d6a4109f
Myron Walker <myron.walker@gmail.com>
2023-03-18 08:00:09 -0700

    Did some work on interfaces.

---
+++ source/packages/mojo/xmods/interfaces/ilandscapedevice.py
@@ -0,0 +1,24 @@
+"""
+.. module:: ilandscapedevice
+    :platform: Darwin, Linux, Unix, Windows
+    :synopsis: Module that contains protocol for a Landscape device
+
+.. moduleauthor:: Myron Walker <myron.walker@gmail.com>
+"""
+
+__author__ = "Myron Walker"
+__copyright__ = "Copyright 2020, Myron W Walker"
+__credits__ = []
+__version__ = "1.0.0"
+__maintainer__ = "Myron Walker"
+__email__ = "myron.walker@gmail.com"
+__status__ = "Development" # Prototype, Development or Production
+__license__ = "MIT"
+
+from typing import Any, List, Protocol
+
+class ILandscapeDevice(Protocol):
+    """
+        The ILandscapeDevice interface is used to provide a common interface for performing
+        basic interop with a landscape device.
+    """
