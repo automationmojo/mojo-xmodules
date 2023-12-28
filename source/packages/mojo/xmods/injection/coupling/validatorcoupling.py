715ea838073e16731ec70d5ca65e4750a77d887e
Myron Walker <myron.walker@gmail.com>
2023-12-27 22:36:37 -0600

    Add attach to flow as the base method for ValidationCoupling

--- source/packages/mojo/xmods/injection/coupling/validatorcoupling.py
+++ source/packages/mojo/xmods/injection/coupling/validatorcoupling.py
@@ -1,5 +1,15 @@
+from typing import Any
+
+from mojo.errors.exceptions import NotOverloadedError
 
 class ValidatorCoupling:
     """
         Base type for Validator objects used to verify return types of validator factory methods.
     """
+
+    def attach_to_test(self, testscope: Any, suffix: str):
+        """
+            The 'attach_to_test' method is called by the sequencer in order to attach the validator to its partner
+            test scope.
+        """
+        raise NotOverloadedError("'ValidatorCoupling' derived objects must overload the 'attach_to_test' method.")
